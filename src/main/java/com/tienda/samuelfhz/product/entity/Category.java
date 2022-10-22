4db071bb79eee38dc30fb2a809b9adef6581852b
SamuelFumeroHdez <samuelfumerohdez@gmail.com>
2022-10-23 00:20:14 +0100

    first commit

---
+++ src/main/java/com/tienda/samuelfhz/product/entity/Category.java
@@ -0,0 +1,25 @@
+package com.tienda.samuelfhz.product.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "SFHZCATEGORY")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+}
